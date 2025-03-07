a7963f8ed7a6e3dad33a41a5a85d26e10284a815
daws-4 <villamizarandresdavid@gmail.com>
2025-03-07 11:55:10 -0400

    07-03-2025

--- app/page.tsx
+++ app/page.tsx
@@ -9,27 +9,57 @@ export default function Home(){
   const item1right = useRef<HTMLDivElement>(null);
 
   // Aplica las animaciones al cargar la página
+  //izquierda
   useEffect(() => {
-    if (item1left.current) {
-      item1left.current.classList.add('animate-slideInFromLeft');
-    }
-    if (item1right.current) {
-      item1right.current.classList.add('animate-slideInFromRight');
-    }
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('animate-slideInFromLeft');
+          }
+        });
+      },
+      { threshold: 0.1 }
+    );
+    if (item1left.current) observer.observe(item1left.current);
+
+    return () => {
+      if (item1left.current) observer.unobserve(item1left.current);
+    };
   }, []);
+  //derecha
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('animate-slideInFromRight');
+          }
+        });
+      },
+      { threshold: 0.1 }
+    );
+
+    if (item1right.current) observer.observe(item1right.current);
+
+    return () => {
+      if (item1right.current) observer.unobserve(item1right.current);
+    };
+  }, []);
+
 //arreglar el crecimiento inesperado de la pantalla
   return(
     <section className=' flex bg-gradient-to-tr from-slate-950 to-customDarkBlue'>
       <div ref={item1left} className='absolute z-10 flex pt-10 px-6'>
         <h1 className={clsx("text-center text-6xl text-customBorder", "justify-start items-center pl-12 font-serif")}>Niños Cantores <br/> del Táchira</h1>
     </div>
-      <div ref={item1right} className='relative z-0 flex justify-end w-full pt-10 pr-6 pb-10'>
+      <div ref={item1right} className=' z-0 flex justify-end w-full pt-10 pr-6 pb-10'>
         <ImageComponent
           src="/coro_principal.jpg" // Ajusta la ruta de la imagen
           alt="Descripción de la imagen"
           width={900}
           height={700}
-          className="rounded-lg shadow-lg"
+          className="rounded-lg shadow-lg fixed"
         />
       </div>
     </section>
